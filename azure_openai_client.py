441a53efeda3cfc5eb901c8d3743e4bd63341368
subhesh922 <subhesh922@gmail.com>
2025-08-15 16:13:56 +0530

    Create azure_openai_client.py

---
+++ azure_openai_client.py
@@ -0,0 +1,14 @@
+#azure_openai_client.py
+import os
+from openai import AzureOpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = AzureOpenAI(
+    api_key=os.getenv("AZURE_OPENAI_API_KEY"),
+    api_version=os.getenv("AZURE_OPENAI_API_VERSION"),
+    azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT")
+)
+
+DEPLOYMENT_NAME = os.getenv("AZURE_OPENAI_DEPLOYMENT")
